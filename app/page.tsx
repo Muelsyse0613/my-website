9af0fe16d5993db9826174bf2104b28f28ab1710
Muelsyse0613 <3307945594@qq.com>
2026-03-29 00:36:23 +0800

    fix：禁用主页缓存

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const revalidate = 0;
+
 export default async function Home() {
   const { data: diaries } = await supabase
     .from('diaries')
